39d48a5999dcfe0f50157fb23472d5cdd2dde5ad
Abhideep Singh <abhi@dothat.in>
2016-12-20 23:04:26 +0530

    Use a default File Info based on Path when there is no authority

--- src/main/java/com/sarality/file/FileUriResolver.java
+++ src/main/java/com/sarality/file/FileUriResolver.java
@@ -1,9 +1,13 @@
 package com.sarality.file;
 
 import android.app.Activity;
-import android.database.Cursor;
 import android.net.Uri;
-import android.provider.MediaStore;
+import android.text.TextUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 /**
  * Resolves a URI into a {@link FileInfo}
@@ -11,6 +15,8 @@ import android.provider.MediaStore;
  * @author abhideep@ (Abhideep Singh)
  */
 public class FileUriResolver {
+
+  public static final Logger logger = LoggerFactory.getLogger(FileUriResolver.class);
 
   private final static UriResolverRegistry REGISTRY = new UriResolverRegistry();
 
@@ -22,6 +28,13 @@ public class FileUriResolver {
 
   public FileInfo resolve(Uri uri) {
     String authority = uri.getAuthority();
+    logger.info("The uri {} has the authority {}", uri.getPath(), authority);
+
+    if (TextUtils.isEmpty(authority)) {
+      File file = new File(uri.getPath());
+      return new FileInfo(file.getPath(), file.getName());
+    }
+
     UriResolver resolver = REGISTRY.getResolver(authority);
     return resolver.resolve(activity, uri);
   }
